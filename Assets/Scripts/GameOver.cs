1a651a39330abe2a16536ba26dc6bd1b807d8528
Beyazit Kaygi <mbyztk200007@gmail.com>
2023-06-28 18:09:58 +0300

    Fading and MenuSettings

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,27 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+public class GameOver : MonoBehaviour {
 
-public class GameOver : MonoBehaviour
-{
-    public TMP_Text roundsText;
+	public TextMeshProUGUI roundsText;
 
-    void OnEnable()
-    {
-        roundsText.SetText(PlayerStats.Rounds.ToString());
+	public string menuSceneName = "MainMenu";
+
+	public SceneFader sceneFader;
+
+	void OnEnable ()
+	{
+		roundsText.text = PlayerStats.Rounds.ToString();
+	}
+
+	public void Retry ()
+	{
+
+		sceneFader.FadeTo(SceneManager.GetActiveScene().name);
     }
 
-    public void Retry()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	public void Menu ()
+	{
+		sceneFader.FadeTo(menuSceneName);
+	}
 
-    }
-
-    public void Menu()
-    {
-        Debug.Log("Go to menu.");
-    }
 }
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,42 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour {
 
-public class PauseMenu : MonoBehaviour
-{
-    public GameObject ui;
+	public GameObject ui;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
-        {
-            Toggle();
-        }
+	public string menuSceneName = "MainMenu";
+
+	public SceneFader sceneFader;
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+		{
+			Toggle();
+		}
+	}
+	public void Toggle ()
+	{
+		ui.SetActive(!ui.activeSelf);
+		if (ui.activeSelf)
+		{
+			Time.timeScale = 0f;
+		} else
+		{
+			Time.timeScale = 1f;
+		}
+	}
+	public void Retry ()
+	{
+		Toggle();
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		sceneFader.FadeTo(SceneManager.GetActiveScene().name);
     }
 
-    public void Toggle()
-    {
-        ui.SetActive(!ui.activeSelf);
-
-        if (ui.activeSelf)
-        {
-            Time.timeScale = 0f;
-        }
-        else
-        {
-            Time.timeScale = 1f;
-        }
-    }
-
-    public void Retry()
-    {
-        Toggle();
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
-
-    public void Menu()
-    {
+	public void Menu ()
+	{
+		Toggle();
         Debug.Log("Go to menu.");
-    }
+		sceneFader.FadeTo(menuSceneName);
+	}
+
 }
