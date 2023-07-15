fbfe064cf0f9192a4ff18fd7923a3c696b27435b
MuazTopalan <a.muaztopalan@gmail.com>
2023-07-16 02:31:36 +0300

    New scene for prefab production (seperately) and cursor updated

---
+++ Assets/Scenes/SALİH/FanOffButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanOffButton : MonoBehaviour
+{
+    public GameObject fanObject; // Reference to the fan game object
+
+    private Animator fanAnimator; // Animator component of the fan object
+
+    private void Start()
+    {
+        // Get the Animator component of the fan object
+        fanAnimator = fanObject.GetComponent<Animator>();
+    }
+
+    private void OnMouseDown()
+    {
+        TurnTheFanOff();
+    }
+
+    private void TurnTheFanOff()
+    {
+        fanAnimator.SetTrigger("turnOff");
+    }
+}
---
+++ Assets/Scenes/SALİH/FanOnButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanOnButton : MonoBehaviour
+{
+    public GameObject fanObject; // Reference to the fan game object
+
+    private Animator fanAnimator; // Animator component of the fan object
+
+    private void Start()
+    {
+        // Get the Animator component of the fan object
+        fanAnimator = fanObject.GetComponent<Animator>();
+    }
+
+    private void OnMouseDown()
+    {
+        TurnTheFanOn();
+    }
+
+    private void TurnTheFanOn()
+    {
+        fanAnimator.SetTrigger("turnOn");
+    }
+}
